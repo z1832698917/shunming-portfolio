6dd3039b014020100ef195eb23a6c4e92c94ce78
z1832698917 <1832698917@qq.com>
2026-03-26 17:22:47 +0800

    Update data.js - read from GitHub

--- js/data.js
+++ js/data.js
@@ -2,45 +2,36 @@
  * Portfolio Data - 从 GitHub 读取，本地 localStorage 作为缓存
  */
 
-(function () {
+(function() {
     'use strict';
 
-    var GITEE = {
-        username: 'z1832698917',
+    var GITHUB = {
+        owner: 'z1832698917',
         repo: 'shunming-portfolio',
         branch: 'main'
     };
 
-    var DEFAULT_DATA = {
-        photography: [],
-        ecommerce: [],
-        typesetting: []
-    };
+    var DEFAULT_DATA = { photography: [], ecommerce: [], typesetting: [] };
 
     async function loadFromGitHub() {
         try {
-            var url = 'https://raw.githubusercontent.com/' + GITEE.username + '/' + GITEE.repo + '/' + GITEE.branch + '/data/portfolio.json?t=' + Date.now();
+            var url = 'https://raw.githubusercontent.com/' + GITHUB.owner + '/' + GITHUB.repo + '/' + GITHUB.branch + '/data/portfolio.json?t=' + Date.now();
             var res = await fetch(url);
             if (!res.ok) return null;
             return await res.json();
-        } catch (e) {
-            return null;
-        }
+        } catch(e) { return null; }
     }
 
     function loadFromCache() {
         try {
             var saved = localStorage.getItem('portfolioData');
             return saved ? JSON.parse(saved) : null;
-        } catch (e) {
-            return null;
-        }
+        } catch(e) { return null; }
     }
 
     async function init() {
         window.PORTFOLIO_DATA = loadFromCache() || DEFAULT_DATA;
         window.dispatchEvent(new CustomEvent('portfolioDataReady', { detail: window.PORTFOLIO_DATA }));
-
         var remote = await loadFromGitHub();
         if (remote) {
             window.PORTFOLIO_DATA = remote;
@@ -49,7 +40,7 @@
         }
     }
 
-    window.refreshPortfolioData = async function () {
+    window.refreshPortfolioData = async function() {
         var remote = await loadFromGitHub();
         if (remote) {
             window.PORTFOLIO_DATA = remote;
@@ -58,12 +49,12 @@
         }
     };
 
-    window.addEventListener('storage', function (e) {
+    window.addEventListener('storage', function(e) {
         if (e.key === 'portfolioData' && e.newValue) {
             try {
                 window.PORTFOLIO_DATA = JSON.parse(e.newValue);
                 window.dispatchEvent(new CustomEvent('portfolioDataUpdated', { detail: window.PORTFOLIO_DATA }));
-            } catch (err) {}
+            } catch(err) {}
         }
     });
 
